91fd15338664389621a627890ba803f00aace3ad
miggazElquez <miguel.gazquez82@gmail.com>
2023-12-10 19:35:44 +0100

    BarnesHut fully implemented (not optimized)

--- src/murb/implem/SimulationNBodyBarnesHut.hpp
+++ src/murb/implem/SimulationNBodyBarnesHut.hpp
@@ -4,10 +4,39 @@
 #include <string>
 
 #include "core/SimulationNBodyInterface.hpp"
+#include "core/Bodies.hpp"
+
+#define THETA 0.5f
+
+struct Octree;
+
+struct internal_node {
+  Octree* children[8];
+};
+
+struct external_node {
+  const dataAoS_t<float>* body;
+};
+
+
+struct Octree {
+  bool internal; //1 for internal node, 0 for external
+  float size;
+  float qx,qy,qz; //Position of the middle of the group.
+  float CoMx,CoMy,CoMz; //Center of mass of the group
+  float mass; //Total mass of the group  
+  union {
+    struct internal_node internal;
+    struct external_node external;
+  } data;
+};
+
 
 class SimulationNBodyBarnesHut : public SimulationNBodyInterface {
   protected:
     std::vector<accAoS_t<float>> accelerations; /*!< Array of body acceleration structures. */
+    Octree *tree;
+    float softSquared;
 
   public:
     SimulationNBodyBarnesHut(const unsigned long nBodies, const std::string &scheme = "galaxy", const float soft = 0.035f,
@@ -18,6 +47,13 @@ class SimulationNBodyBarnesHut : public SimulationNBodyInterface {
   protected:
     void initIteration();
     void computeBodiesAcceleration();
+    void getBoundingBox(float*,float*,float*,float*,float*,float*);
+    void insertBody(Octree* tree, const dataAoS_t<float> *body);
+    void updateTree(Octree* tree);
+    void computeOctree();
+    void freeTree(Octree *tree);
+    void computeBodyAcceleration(Octree *tree,const dataAoS_t<float> *body,float *ax, float *ay, float *az, int depth);
+
 };
 
 #endif /* SIMULATION_N_BODY_OPTIM_HPP_ */
