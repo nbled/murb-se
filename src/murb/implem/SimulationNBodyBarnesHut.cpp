11e81a6611502baf302cf2f7d3323f87d77c2b43
miggazElquez <miguel.gazquez82@gmail.com>
2023-12-10 19:35:44 +0100

    BarnesHut fully implemented (not optimized)

--- src/murb/implem/SimulationNBodyBarnesHut.cpp
+++ src/murb/implem/SimulationNBodyBarnesHut.cpp
@@ -13,6 +13,23 @@ SimulationNBodyBarnesHut::SimulationNBodyBarnesHut(const unsigned long nBodies, 
 {
     this->flopsPerIte = 30.f * ((float)this->getBodies().getN() * (float)this->getBodies().getN() - (float)this->getBodies().getN())/2;
     this->accelerations.resize(this->getBodies().getN());
+    this->softSquared = this->soft * this->soft;
+}
+
+void SimulationNBodyBarnesHut::freeTree(Octree *tree) {
+    if(tree) {
+        if (tree->internal == false) {
+            free(tree);
+            return;
+        }
+
+        for (int i=0;i<8;i++) {
+            
+            this->freeTree(tree->data.internal.children[i]);
+        }
+
+        free(tree);
+    }
 }
 
 void SimulationNBodyBarnesHut::initIteration()
@@ -22,49 +39,233 @@ void SimulationNBodyBarnesHut::initIteration()
         this->accelerations[iBody].ay = 0.f;
         this->accelerations[iBody].az = 0.f;
     }
+    this->computeOctree();
 }
+void SimulationNBodyBarnesHut::getBoundingBox(float *min_x_,float *max_x_,float *min_y_,float *max_y_,float *min_z_,float *max_z_) {
+    const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
+    float min_x = d[0].qx, max_x = d[0].qx, min_y = d[0].qy, max_y = d[0].qy, min_z = d[0].qz, max_z = d[0].qz;
 
-void SimulationNBodyBarnesHut::computeBodiesAcceleration()
-{
-    const dataSoA_t<float> &d = this->getBodies().getDataSoA();
-
-    // compute e²
-    const float softSquared = std::pow(this->soft, 2); // 1 flops
     unsigned long n_bodies = this->getBodies().getN();
 
-    // flops = n² * 20
-    for (unsigned long iBody = 0; iBody < n_bodies; iBody++) {
-        // flops = n * 20
-        for (unsigned long jBody = iBody+1; jBody < n_bodies; jBody++) {
-            const float rijx = d.qx[jBody] - d.qx[iBody]; // 1 flop
-            const float rijy = d.qy[jBody] - d.qy[iBody]; // 1 flop
-            const float rijz = d.qz[jBody] - d.qz[iBody]; // 1 flop
+    for (unsigned long i=0;i<n_bodies;i++) {
+        if (d[i].qx > max_x)
+            max_x = d[i].qx;
+        if (d[i].qx < min_x)
+            min_x = d[i].qx;
+        if (d[i].qy > max_y)
+            max_y = d[i].qy;
+        if (d[i].qy < min_y)
+            min_y = d[i].qy;
+        if (d[i].qz > max_z)
+            max_z = d[i].qz;
+        if (d[i].qz < min_z)
+            min_z = d[i].qz;
+    }
 
-            // compute the || rij ||² distance between body i and body j
+    *min_x_ = min_x;
+    *max_x_ = max_x;
+    *min_y_ = min_y;
+    *max_y_ = max_y;
+    *min_z_ = min_z;
+    *max_z_ = max_z;
+
+
+}
+
+void SimulationNBodyBarnesHut::insertBody(Octree* tree, const dataAoS_t<float> *body) {
+    if (tree->internal == false) { //End case : external node
+        if (tree->data.external.body == NULL) {
+            tree->data.external.body = body;
+            tree->CoMx = body->qx;   //update right now CoM and mass
+            tree->CoMy = body->qy;
+            tree->CoMz = body->qz;
+            tree->mass = body->m;
+        } else {
+            const dataAoS_t<float> *other_body = tree->data.external.body;
+            // printf("body 1 : %e %e %e\n",body->qx,body->qy,body->qz);
+            // printf("body 2 : %e %e %e\n",other_body->qx,other_body->qy,other_body->qz);
+
+            tree->internal = true;
+            // printf("constructing child %e\n",tree->size);
+            for (int i=0;i<8;i++) {
+                Octree *child = (Octree*) malloc(sizeof(Octree));
+                child->internal = false; //Newly created child are external
+                child->size = tree->size / 2;
+                const float size_increment = child->size / 2;
+                child->mass = 0; //we put the mass at 0 to simplify all later computation
+                if (i % 2) { //new_x > x
+                    child->qx = tree->qx + size_increment;
+                } else {
+                    child->qx = tree->qx - size_increment;
+                }
+
+                if (i % 4 > 1) {
+                    child->qy = tree->qy + size_increment;
+                } else {
+                    child->qy = tree->qy - size_increment;
+                }
+
+                if (i > 3) {
+                    child->qz = tree->qz + size_increment;
+                } else {
+                    child->qz = tree->qz - size_increment;
+                }
+
+                child->data.external.body = NULL;
+                tree->data.internal.children[i] = child;
+            }
+
+            this->insertBody(tree,other_body); //Now that we have changed the type of the node we reinsert the original body, and the new one
+            this->insertBody(tree,body);       //They will go on to the good external node.
+
+        }
+        return;
+    }
+    //we go to the next group.
+    int index = 0;
+    index += (body->qx > tree->qx) * 1; //Permet de donner un indice différent selon tout les cas, de 0 à 7
+    index += (body->qy > tree->qy) * 2;
+    index += (body->qz > tree->qz) * 4;
+
+    // printf("center : %e %e %e\n",tree->qx,tree->qy,tree->qz);
+    // printf("%e %e %e going to %d\n",body->qx,body->qy,body->qz,index);
+
+    this->insertBody(tree->data.internal.children[index], body);
+}
+
+void SimulationNBodyBarnesHut::updateTree(Octree* tree) { //Could probably be fused with `insertBody`
+    if (tree->internal == false) 
+        return;
+
+
+    float CoMx = 0,CoMy=0, CoMz=0, mass = 0;
+    for (int i=0;i<8;i++) {
+        this->updateTree(tree->data.internal.children[i]);
+        float child_mass = tree->data.internal.children[i]->mass;
+        CoMx += tree->data.internal.children[i]->CoMx * child_mass;
+        CoMy += tree->data.internal.children[i]->CoMy * child_mass;
+        CoMz += tree->data.internal.children[i]->CoMz * child_mass;
+        mass += child_mass;
+
+    }
+
+    CoMx /= mass;
+    CoMy /= mass;
+    CoMz /= mass;
+    tree->mass = mass;
+    tree->CoMx = CoMx;
+    tree->CoMy = CoMy;
+    tree->CoMz = CoMz;
+}
+
+void SimulationNBodyBarnesHut::computeOctree() {
+    float min_x,max_x,min_y,max_y,min_z,max_z;
+    this->getBoundingBox(&min_x,&max_x,&min_y,&max_y,&min_z,&max_z);
+
+    float size_x = max_x - min_x;
+    float size_y = max_y - min_y;
+    float size_z = max_z - min_z;
+
+    float size;
+    if (size_x > size_y)
+        size = size_x;
+    else
+        size = size_y;
+
+    if (size_z > size)
+        size = size_z;
+
+
+    this->tree = (Octree*) malloc(sizeof(Octree));
+    this->tree->size = size;
+    this->tree->qx = (max_x - min_x) / 2 + min_x;
+    this->tree->qy = (max_y - min_y) / 2 + min_y;
+    this->tree->qz = (max_z - min_z) / 2 + min_z;
+    this->tree->internal = false;
+    this->tree->data.external.body = NULL;
+    // printf("bouding box : %e %e,  %e %e,  %e %e\n",min_x,max_x,min_y,max_y,min_z,max_z);
+    // printf("Created tree of size %e, at %e %e %e\n",size,this->tree->qx,this->tree->qy,this->tree->qz );
+
+    const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
+
+    unsigned long n_bodies = this->getBodies().getN();
+    for (unsigned long i=0;i<n_bodies;i++) {
+        // printf("inserting body %e %e %e\n",d[i].qx,d[i].qy,d[i].qz);
+        this->insertBody(this->tree,&d[i]);
+    }
+
+    this->updateTree(this->tree);
+
+
+}
+
+void SimulationNBodyBarnesHut::computeBodyAcceleration(Octree *tree,const dataAoS_t<float> *body,float *ax, float *ay, float *az, int depth) {
+    // printf("    entering node %e %e %e , size=%e, depth=%d\n",tree->qx,tree->qy,tree->qz,tree->size,depth);
+    if (tree->internal == false) {
+        if (tree->mass != 0) { 
+            // printf("        with body %e %e %e of mass %e\n",tree->CoMx,tree->CoMy,tree->CoMz,tree->mass);
+            const float rijx = tree->CoMx - body->qx; // 1 flop
+            const float rijy = tree->CoMy - body->qy; // 1 flop
+            const float rijz = tree->CoMz - body->qz; // 1 flop
+
             const float rijSquared = rijx * rijx + rijy * rijy + rijz * rijz; // 5 flops
 
             // compute the acceleration value between body i and body j: || ai || = G.mj / (|| rij ||² + e²)^{3/2}
             const float x = this->G / ((rijSquared + softSquared) * std::sqrt(rijSquared + softSquared));
-            const float ai = x * d.m[jBody]; // 1 flops
-            const float aj = x * d.m[iBody]; // 1 flops
+            const float ai = x * tree->mass; // 1 flops
 
-            // add the acceleration value into the acceleration vector: ai += || ai ||.rij
-            this->accelerations[iBody].ax += ai * rijx; // 2 flops
-            this->accelerations[iBody].ay += ai * rijy; // 2 flops
-            this->accelerations[iBody].az += ai * rijz; // 2 flops
-
-            this->accelerations[jBody].ax += aj * -rijx; // 2 flops
-            this->accelerations[jBody].ay += aj * -rijy; // 2 flops
-            this->accelerations[jBody].az += aj * -rijz; // 2 flops
-
+            *ax += ai * rijx;
+            *ay += ai * rijy;
+            *az += ai * rijz;
+            // printf("    ax += %e  = %e\n",ai * rijx, *ax);
+        } else {
+            // printf("        empty\n");
         }
+    } else {
+        const float rijx = tree->CoMx - body->qx; // 1 flop
+        const float rijy = tree->CoMy - body->qy; // 1 flop
+        const float rijz = tree->CoMz - body->qz; // 1 flop
+
+        const float rijSquared = rijx * rijx + rijy * rijy + rijz * rijz; // 5 flops
+
+        const float s = tree->size * tree->size;
+
+        if (s / rijSquared <= THETA * THETA) { // s/d < theta <=> s^2 / d^2 < theta^2, because everything is >= 0
+            // printf("        (d=%d) with group %e %e %e of mass %e\n",depth,tree->CoMx,tree->CoMy,tree->CoMz,tree->mass);
+            const float x = this->G / ((rijSquared + softSquared) * std::sqrt(rijSquared + softSquared));
+            const float ai = x * tree->mass; // 1 flops
+
+            *ax += ai * rijx;
+            *ay += ai * rijy;
+            *az += ai * rijz;
+
+        } else { //We don't use this group
+            // printf("        (d=%d) we go see the childs\n",depth);        
+            for (int i=0;i<8;i++) {
+                this->computeBodyAcceleration(tree->data.internal.children[i],body,ax,ay,az,depth + 1);
+            }
+        }
+    }
+}
+
+void SimulationNBodyBarnesHut::computeBodiesAcceleration()
+{
+    const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
+
+    unsigned long n_bodies = this->getBodies().getN();
+
+    for (unsigned long iBody = 0; iBody < n_bodies; iBody++) {
+        // printf("Computing for %e %e %e\n",d[iBody].qx,d[iBody].qy,d[iBody].qz);
+        this->computeBodyAcceleration(this->tree,&d[iBody],&this->accelerations[iBody].ax,&this->accelerations[iBody].ay,&this->accelerations[iBody].az,0);
     }
 }
 
 void SimulationNBodyBarnesHut::computeOneIteration()
 {
+    // printf("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
     this->initIteration();
     this->computeBodiesAcceleration();
+    this->freeTree(this->tree);
     // time integration
     this->bodies.updatePositionsAndVelocities(this->accelerations, this->dt);
 }
